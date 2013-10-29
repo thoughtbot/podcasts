72ab7d493295b428ccb770d64f188d584dc2b3d6
Chad Pytel <cpytel@thoughtbot.com>
2013-10-29 16:11:35 -0400

    clean up login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
+  devise :database_authenticatable,
+    :rememberable,
+    :trackable,
+    :validatable,
+    :omniauthable,
+    :omniauth_providers => [:google_oauth2]
 
   def self.find_or_create_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
