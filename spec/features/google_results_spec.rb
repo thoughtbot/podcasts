973b658de8420f01a0e7f834b5774efc11e3708d
Dan Croak <dan@thoughtbot.com>
2013-12-29 14:15:19 -0800

    Improve page titles for SEO
    Google rewrites page titles in search results:
    
    https://support.google.com/webmasters/answer/35624?hl=en
    
    See the Trello card for screenshots of podcasts.thoughtbot.com page
    titles being re-written in Google search results:
    
    https://trello.com/c/3rO5yDi2
    
    Since we can rely on Google adding "thoughtbot" and "podcast" to the
    page title for search queries, this commit simplifies our page title
    strategy:
    
    * Home page: "thoughtbot podcasts"
    * Podcast page: "Giant Robots Smashing Into Other Giant Robots Podcast"
    * Episode page: "CoreFrustration (Reda Lemeden)"
    
    Since we don't need Flutie's `page_title` method, and weren't using it's
    other method, `body_class`, it can be removed.
    
    This commit also fixes a `yield(:meta_description) ||` problem by using
    `yield(:meta_description).presence ||`. `yield` without a `content_for`
    returns an empty string, no `nil`.
    
    All page title and meta description specs have been consolidated in the
    spec/features/google_results_spec.rb file.

--- spec/features/google_results_spec.rb
+++ spec/features/google_results_spec.rb
@@ -1,20 +1,33 @@
 require 'spec_helper'
 
-describe 'Google results' do
-  it 'includes podcast page with short description' do
+describe 'Google results include' do
+  it 'home page with title and description' do
+    title = I18n.t('layouts.app_name')
+    description = I18n.t('layouts.description')
+    selector = "meta[name='description'][content='#{description}']"
+
+    visit '/'
+
+    expect(page).to have_title(title)
+    expect(page).to have_css(selector, visible: false)
+  end
+
+  it 'podcast page with title and description' do
     show = create(:show, short_description: 'Short')
 
     visit show_episodes_path(show)
 
+    expect(page).to have_title(show.title)
     expect(page).
       to have_css("meta[name='description'][content='Short']", visible: false)
   end
 
-  it 'includes episode page with description' do
+  it 'episode page with title and description' do
     episode = create(:episode, description: 'Listen')
 
     visit show_episode_path(episode.show, episode)
 
+    expect(page).to have_title(episode.title)
     expect(page).
       to have_css("meta[name='description'][content='Listen']", visible: false)
   end
